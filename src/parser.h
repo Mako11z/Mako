f56e9beacd79bc6f49afd3854a20a402819bb4ac
Isaiah Gage <frb32@txstate.edu>
2024-06-27 01:04:26 -0500

    Updated vector of children so eahc node has a vector of children if needed

--- src/parser.h
+++ src/parser.h
@@ -1,46 +1,3 @@
-/* Each node is a :
-    - BinaryExpression
-    - IfStatements
-    - While loop
-    - Print()
-    - VariableDef
-    - Variable
-    - Literal
-    - Assignment
-
-    Example:
-    if (a < b) {int i = 0;}
-    - We see a if statemnet so we create a Ifstatement ASTNode
-        - We expect a '(' after the if statement and a ')' at the end
-    - We consume the 'a'
-        - Check if 'a' is defined
-    - The we see the '<' so we create a BinaryExpression ASTNode with '<' as the operator
-    - Then we see the 'b' and we must also check if it is defined as well
-    - A ')' is encountered so we expect a open and clsoing '{}'
-    - We then see int so we create a VariableDef ASTNode
-    - 'i' is encountered and we update the VariableDef ASTNode to include the variable name 'i'
-    - '=' is encountered which indicates 'i' is being initilized and we parse the next value
-    - 0 is what we consume next and it is a literal so we create a Literal ASTNode for this value
-    - We attach the Literal ASTNode as the child to the VariableDef ASTNode
-        - Indicates 'i' = 0
-    - Consume the ';' confirming the end of the variable declariation
-
-    Example:
-    y = 5 + 10 * 2
-    - Recognize y as a variable name
-    - We create a VariableNode to rrepresent 'y'
-    - Encounter '=' so we create a Assignment AASTNode
-    - The Variable node will be it's left child
-    - Then we see 5 as a literal and create a literal node
-    - Encounter '+' so we create a BinaryExpression node with '+' as the operator
-    - 5 will be it's left child
-    - Encounter 10 and create a Literal node
-    - '*' multiplcation is encountered and we create another BinaryExpression node
-    - The 10 will be it's left child
-    - 2 is encountered and we create a Literal node
-    - This will be the right child
-    - Attach the BinaryExpression as the right child of Assignmemt node
-*/
 #ifndef PARSER_H
 #define PARSER_H
 
@@ -62,9 +19,8 @@ enum class ASTNodeType
 struct ASTNode
 {
     ASTNodeType type;
-    ASTNode *left;
-    ASTNode *right;
-    ASTNode(ASTNodeType t) : type(t), left(nullptr), right(nullptr) {}
+    std::vector<ASTNode *> children;
+    ASTNode(ASTNodeType t) : type(t) {}
     virtual ~ASTNode() = default;
 };
 
@@ -80,8 +36,8 @@ struct BinaryExpressionNode : ASTNode
     std::string operation;
     BinaryExpressionNode(std::string &op, ASTNode *l, ASTNode *r) : ASTNode(ASTNodeType::BinaryExpression), operation(op)
     {
-        left = l;
-        right = r;
+        children.push_back(l);
+        children.push_back(r);
     }
 };
 
@@ -95,27 +51,23 @@ struct AssignmemtNode : ASTNode
 {
     AssignmemtNode(ASTNode *l, ASTNode *r) : ASTNode(ASTNodeType::Assignment)
     {
-        left = l;
-        right = r;
+        children.push_back(l);
+        children.push_back(r);
     }
 };
 
 struct IfStatementNode : ASTNode
 {
-    IfStatementNode(ASTNode *l, ASTNode *r) : ASTNode(ASTNodeType::IfStatement)
-    {
-        left = l;
-        right = r;
-    }
+    std::vector<ASTNode *> conditions;
+    std::vector<ASTNode *> body;
+    IfStatementNode() : ASTNode(ASTNodeType::IfStatement) {}
 };
 
 struct WhileLoopNode : ASTNode
 {
-    WhileLoopNode(ASTNode *l, ASTNode *r) : ASTNode(ASTNodeType::WhileLoop)
-    {
-        left = l;
-        right = r;
-    }
+    std::vector<ASTNode *> conditions;
+    std::vector<ASTNode *> body;
+    WhileLoopNode() : ASTNode(ASTNodeType::WhileLoop) {}
 };
 
 struct PrintNode : ASTNode
@@ -133,6 +85,21 @@ public:
 private:
     std::vector<Token> &tokens;
     size_t current_index;
+    Token peekToken()
+    {
+        if (current_index + 1 < tokens.size())
+        {
+            return tokens[current_index + 1];
+        }
+        else
+        {
+            return Token{TokenType::Punctuation, ""};
+        }
+    }
+    void advanceToken()
+    {
+        current_index++;
+    }
 };
 
 #endif
